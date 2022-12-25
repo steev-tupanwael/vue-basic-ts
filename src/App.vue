1a6f2d3e10fae90d73c22638cfe405bf726c7d15
steev tupanwael <steev@192.168.1.16>
2022-12-25 17:47:32 +0900

    feat(component): create hello component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import hello from './components/hello.vue'
+
+let teks = ref("")
 </script>
 
 <template>
+  <input type="text" class="bg-slate-200" v-model=teks onfocus>
   <div class="text-3xl font-sans font-medium">
-    HelloWorld
+    <hello :content="teks" />
   </div>
 </template>
---
+++ src/components/hello.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  interface Message {
+    content: string
+  }
+
+  const message = defineProps<Message>()
+</script>
+
+<template>
+  <h1>Hello {{ message.content }}</h1>
+</template>
